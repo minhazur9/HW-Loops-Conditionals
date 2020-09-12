04dae7a19b9d60a0047b6ee914cbf1eb5480ff04
min <minhazur9@gmail.com>
2020-09-11 23:50:16 -0400

    Commit 4 - Fizz Buzz answered

--- scripts/app.js
+++ scripts/app.js
@@ -16,18 +16,35 @@
 // 1) Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
 // 2) For every even number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
 
-for (let i = 1; i <= 20; i++) {
-    if(i % 2 == 0) {    
-       switch(Math.floor(Math.random()*3)){
-         case 0: console.log("...human...why you taking pictures of me?...");
-         break;
-         case 1: console.log("...the catnip made me do it...");
-         break;
-         case 2: console.log("...why does the red dot always get away...");
-       }
+// for (let i = 1; i <= 20; i++) {
+//     if(i % 2 === 0) {    
+//        switch(Math.floor(Math.random()*3)){
+//          case 0: console.log("...human...why you taking pictures of me?...");
+//          break;
+//          case 1: console.log("...the catnip made me do it...");
+//          break;
+//          case 2: console.log("...why does the red dot always get away...");
+//        }
+//     }
+//     else {
+//       console.log('Love me, pet me! HSSSSSS!');
+//     }
+// }
+
+// Fizz Buzz
+// 1) If a number is divisible by 3 log "Fizz" instead of the number.
+// 2) If a number is divisible by 5 log "Buzz" instead of the number
+// 3) If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number, "Fizz", or "Buzz".
+
+for(let i = 1; i <= 100; i++){
+    if((i % 3 === 0) && (i % 5 === 0)){
+        console.log("FizzBuzz");
     }
-    else {
-      console.log('Love me, pet me! HSSSSSS!');
+    else if(i % 3 === 0){
+        console.log("Fizz");
+    }
+    else if(i % 5 === 0){
+        console.log("Buzz");
     }
 }
 
